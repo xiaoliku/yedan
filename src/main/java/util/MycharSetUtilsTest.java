7dc459876f2de4d064229faafb6b9a8d02729dd9
yedan <1139169914@qq.com>
2019-11-30 01:36:05 +0800

    更新说明

--- src/main/java/util/MycharSetUtilsTest.java
+++ src/main/java/util/MycharSetUtilsTest.java
@@ -1,16 +1,11 @@
 package util;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class MycharSetUtilsTest extends MycharSetUtils {
     private MycharSetUtils MS = new MycharSetUtils ();
